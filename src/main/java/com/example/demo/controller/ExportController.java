862a2e0b351abddd4b3716e733cab2926e4c9fe2
GermainPinganaud <germain.pinganaud@gmail.com>
2019-02-22 14:08:53 +0100

    added xlsx

--- src/main/java/com/example/demo/controller/ExportController.java
+++ src/main/java/com/example/demo/controller/ExportController.java
@@ -2,6 +2,11 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Client;
 import com.example.demo.service.ClientService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +19,6 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -44,5 +48,20 @@ public class ExportController {
             );
         }
     }
+    @GetMapping("/clients/xlsx")
+    public void clientsXLSX(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/xlsx");
+        response.setHeader("Content-Disposition", "attachment; filename=\"clients.xlsx\"");
+        List<Client> allClients = clientService.findAllClients();
+        LocalDate now = LocalDate.now();
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Clients");
+        Row headerRow = sheet.createRow(0);
+        Cell cellPrenom = headerRow.createCell(0);
+        cellPrenom.setCellValue("Prénom");
+        workbook.write(response.getOutputStream());
+        workbook.close();
 
+
+    }
 }
